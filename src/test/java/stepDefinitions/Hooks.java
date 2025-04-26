0e87f2b92e2240ac4628bb3513749294f56efa59
noushadpathan1 <noushad.pathan@eigital.com>
2025-04-26 21:22:24 +0530

    added log on extent report

---
+++ src/test/java/stepDefinitions/Hooks.java
@@ -0,0 +1,14 @@
+package stepDefinitions;
+
+import com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter;
+import io.cucumber.java.After;
+import static resources.Utils.requestResponseLogs;
+
+public class Hooks {
+
+    @After
+    public void afterScenario() {
+        // Log API request and response details into the Extent Report for the current test step
+        ExtentCucumberAdapter.addTestStepLog(requestResponseLogs.toString());
+    }
+}
